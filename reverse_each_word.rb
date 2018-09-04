de14fb1c502879d61955687de8b4e19281832a3c
Steven Zapata <szapata427@gmail.com>
2018-09-04 17:31:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(string)
   array = string.split(" ")
   
   new_array = array.each do |word|
-     reverse = word.reverse
-     return array.join(" ")
+     word.reverse
+     
   end
-  
+  return new_array.join(" ")
   end
     
